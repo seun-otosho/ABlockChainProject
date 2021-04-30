19a9bc43ea7f22bf84f2a28b1e74d8b7e119fb91
Seun Omotosho <seun@kolacredit.com>
2021-04-30 18:49:47 +0100

    Blockchain mining and validation

--- blockchain.py
+++ blockchain.py
@@ -3,8 +3,8 @@ from hashlib import sha256
 import snoop
 from loguru import logger
 
-
 from heartrate import trace
+
 trace(browser=True)
 
 
@@ -33,23 +33,32 @@ class BlockChain():
         self.chain = chain
 
     def add(self, block):
-        self.chain.append({
-            "hash": block.hash(), "previous": block.previous_hash, "data": block.data, "nonce": block.nonce,
-        })
+        self.chain.append(block)
+
+    def remove(self, block):
+        self.chain.remove(block)
 
     def mine(self, block):
         try:
-            block.previous_hash = self.chain[-1].get("hash")
+            block.previous_hash = self.chain[-1].hash()
         except IndexError:
             pass
 
         while True:
             if block.hash()[:self.difficulty] == "0" * self.difficulty:
-                print(block)
-                self.add(block)
+                self.add(block);
                 break
             else:
-                block.nonce+=1
+                block.nonce += 1
+
+    def is_valid(self):
+        for i in range(1, len(self.chain)):
+            _previous = self.chain[i].previous_hash
+            _current = self.chain[i - 1].hash()
+            if _previous != _current or _current[:self.difficulty] != "0" * self.difficulty:
+                return False
+        return True
+
 
 # @snoop
 @logger.catch
@@ -59,11 +68,17 @@ def main():
     num = 0
 
     for data in database:
-        num+=1
+        num += 1
         blockchain.mine(Block(data, num))
 
     for block in blockchain.chain:
         print(block)
+        print("^"*128)
+
+    blockchain.chain[2].data = "Corrupted"
+    blockchain.mine(blockchain.chain[2])
+
+    print(blockchain.is_valid())
 
 
 if __name__ == '__main__':
